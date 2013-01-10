93d461ff9c8b5a3fdbe32f319976ec7fbae4d234
Tom Pepels <tpepels@gmail.com>
2013-01-10 11:04:43 +0100

    deleted

--- include/gpc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/csm-git/sm/lib/gpc
-# Build directory: /home/tom/csm-git/sm/lib/gpc
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
