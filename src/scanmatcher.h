67ebe295dabad17e5b16af86bc06698a749dcf16
cliffaschet <clifflaschet@hotmail.com>
2013-01-10 11:05:01 +0100

    update to scanmatcher

--- src/scanmatcher.h
+++ src/scanmatcher.h
@@ -31,8 +31,14 @@ class ScanMatcher {
   //Pose of the base frame in fixed frame;
   tf::Transform fixed_to_base;
   //Pose of the last keyframe scan in fixed frame
-  tf::Transform fixed_to_base_keyframe
+  tf::Transform fixed_to_base_keyframe;
+  //Used to convert poses between base of the robot and the laser scanner.
+  tf:Transform laser_to_base;
+  tf::Transform base_to_laser;
+  //
+  ros::Time last_time;
 
+  bool setBasetoLaserTransform(const std::string& frame_id);
   void convertScantoDLP(const sensor_msgs::LaserScan::ConstPtr& scan, LDP& ldp);
   void processScan(LDP& ldp, ros::Time& time);
   void scanMatch(const sensor_msgs::LaserScan::ConstPtr& scan, ros::Time& time);
