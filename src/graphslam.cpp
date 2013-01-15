bff1bb4cbab083bb91ae76f381f2e2189009f5ed
tpepels <tpepels+git@gmail.com>
2013-01-15 20:06:08 +0100

    ComputeR!

--- src/graphslam.cpp
+++ src/graphslam.cpp
@@ -16,7 +16,7 @@ GraphSlam::GraphSlam(ros::NodeHandle& nh) {
 	//
 	nh.param("resolution", resolution, 0.05);
     nh.param("solve_iterations", solve_iterations, 10);
-    nh.param("min_dist", min_dist, 0.20);
+    nh.param("min_dist", min_dist, 0.1);
     nh.param("min_rot", min_rot, 0.3);
     nh.param("solve_after_nodes", solve_after_nodes, 10);
     nh.param("laser_range_t", range_t, 0.9);
@@ -109,8 +109,8 @@ void GraphSlam::spin() {
 			nav_msgs::OccupancyGrid cur_map;
 			graph->generateMap(cur_map);
 			//
-			// if(graph->node_list.size() > 2 && graph->node_list.size() % solve_after_nodes == 0)
-			//	graph->solve(solve_iterations);
+			if(graph->node_list.size() > 2 && graph->node_list.size() % solve_after_nodes == 0)
+				graph->solve(solve_iterations);
 			// ROS_INFO("GraphSlam Map generated");
 			map_publish.publish(cur_map);
 			this->drawPoses();
